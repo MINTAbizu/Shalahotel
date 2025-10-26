61497ce49e3b0871dd36fc106cad502978a70190
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2025-10-26 05:14:03 -0700

    user route and controller

---
+++ Backend/route/user/user.route.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const route=express.Router()
+
+import { usercontroler } from '../../controller/user/user.controller.js'
+
+route.post('/register',usercontroler)
+route.post('/login',usercontroler)
+route.get('/getuser',usercontroler)
+route.put('/updateuser',usercontroler)
+route.delete('/deleteuser',usercontroler)
+route.patch('/changepassword',usercontroler)
+
+
+
+export default route
+
+
